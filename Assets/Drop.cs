8fe4e9d0b64703dc7fefcc2a7328a76ff8e4122c
Jhonatan1337 <jhonatanjovanny@gmail.com>
2021-03-11 02:03:39 -0600

    Inicio del Proyecto

---
+++ Assets/Drop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropandDrag : MonoBehaviour, IDropHandler
+{
+    public GameObject item;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (item != null && item.transform.parent != transform)
+        {
+            item = null;
+        }
+    }
+
+    //___________________________________Drop__________________________________________
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+        {
+            item = MenuManager.itemDragging;
+            item.transform.SetParent(transform);
+            item.transform.position = transform.position;
+
+        }
+    }
+    //_________________________________________________________________________________
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using System;
+
+public class MenuManager : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+    public static GameObject itemDragging;
+
+    Vector3 startPosition;
+    Transform startParent;
+    Transform dragParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dragParent = GameObject.FindGameObjectWithTag("DragParent").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //________________________________Cambio de Escena_________________________________
+    public void EscenaMenu()
+    {
+        SceneManager.LoadScene("Jugadores");
+    }
+
+    public void CargarNivel(string nombreNivel)
+    {
+        SceneManager.LoadScene(nombreNivel);
+    }
+    //_________________________________________________________________________________
+
+    //_____________________________________Drag________________________________________
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        //Mueve el sprite a la posición del mouse
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        itemDragging = null;
+        if (transform.parent == dragParent)
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        itemDragging = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        transform.SetParent(dragParent);
+    }
+    //_________________________________________________________________________________
+
+}  
